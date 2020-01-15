9339023f6f61c668f375fd965bf6df95b2cbe0af
Christophe Diederichs <chris.d.itunes@gmail.com>
2020-01-15 11:56:59 +0100

    benchmarking

--- test.js
+++ test.js
@@ -4,38 +4,38 @@ const ref = require('js-sha512').sha512
 const sodium = require('sodium-native')
 const vectors = require('./vectors.json')
 
-// // timing benchmark
-// {
-//   const buf = Buffer.alloc(8192)
-//   sodium.randombytes_buf(buf)
+// timing benchmark
+{
+  const buf = Buffer.alloc(8192)
+  sodium.randombytes_buf(buf)
 
-//   const hash = sha512()
-//   const jsHash = ref.create()
-//   const refHash = crypto.createHash('sha512') 
+  const hash = sha512()
+  const jsHash = ref.create()
+  const refHash = crypto.createHash('sha512') 
 
-//   console.time('wasm')
-//   for (let i = 0; i < 1000; i++) {
-//     hash.update(buf)
-//   }
-//   const res = hash.digest('hex')
-//   console.timeEnd('wasm')
+  console.time('wasm')
+  for (let i = 0; i < 1000; i++) {
+    hash.update(buf)
+  }
+  const res = hash.digest('hex')
+  console.timeEnd('wasm')
 
-//   console.time('js')
-//   for (let i = 0; i < 1000; i++) {
-//     jsHash.update(buf)
-//   }
-//   const jsRes = jsHash.hex()
-//   console.timeEnd('js')
+  console.time('js')
+  for (let i = 0; i < 1000; i++) {
+    jsHash.update(buf)
+  }
+  const jsRes = jsHash.hex()
+  console.timeEnd('js')
 
-//   console.time('native')
-//   for (let i = 0; i < 1000; i++) {
-//     refHash.update(buf)
-//   }
-//   const refRes = refHash.digest('hex')
-//   console.timeEnd('native')
+  console.time('native')
+  for (let i = 0; i < 1000; i++) {
+    refHash.update(buf)
+  }
+  const refRes = refHash.digest('hex')
+  console.timeEnd('native')
 
-//   console.log('\nhashes are consistent: ', res === refRes && res === jsRes)
-// }
+  console.log('\nhashes are consistent: ', res === refRes && res === jsRes)
+}
 
 // // naive input fuzz
 // const bugs = []
@@ -78,28 +78,28 @@ const vectors = require('./vectors.json')
 // console.log('\nthese test vectors failed: ', failed)
 
 // several instances updated simultaneously
-{
-  const hash1 = sha512() 
-  const hash2 = sha512()
-  const refHash = crypto.createHash('sha512')
+// {
+//   const hash1 = sha512() 
+//   const hash2 = sha512()
+//   const refHash = crypto.createHash('sha512')
 
-  hash1.update('abc')
-  hash2.update('abc')
-  refHash.update('abc')
+//   hash1.update('abc')
+//   hash2.update('abc')
+//   refHash.update('abc')
 
-  hash2.update('defghj')
-  hash1.update('defghj')
-  refHash.update('defghj')
+//   hash2.update('defghj')
+//   hash1.update('defghj')
+//   refHash.update('defghj')
 
-  hash1.update('12egj4')
-  hash2.update('12egj4')
-  refHash.update('12egj4')
+//   hash1.update('12egj4')
+//   hash2.update('12egj4')
+//   refHash.update('12egj4')
 
-  hash1.update('skhdbkbks')
-  hash2.update('skhdbkbks')
-  refHash.update('skhdbkbks')
+//   hash1.update('skhdbkbks')
+//   hash2.update('skhdbkbks')
+//   refHash.update('skhdbkbks')
 
-  console.log(hash1.digest('hex'))
-  console.log(hash2.digest('hex'))
-  console.log(refHash.digest('hex'))
-}
+//   console.log(hash1.digest('hex'))
+//   console.log(hash2.digest('hex'))
+//   console.log(refHash.digest('hex'))
+// }
