3abd5bffd644bced578c3aefaaff6d2b4bc8fbb3
Christophe Diederichs <chris.d.itunes@gmail.com>
2020-02-18 15:34:09 +0100

    tape tests added

--- test/index.js
+++ test/index.js
@@ -1,7 +1,15 @@
 const sha512 = require('../')
 const crypto = require('crypto')
+const tape = require('tape')
 const sha512js = require('js-sha512').sha512
 const vectors = require('./vectors.json')
+
+tape('empty input', function (t) {
+  const hash = sha512().digest('hex')
+  const ref = crypto.createHash('sha512').digest('hex')
+  t.equal(hash, ref, 'consistent for empty input')
+  t.end()
+})
 
 // timing benchmark
 {
@@ -36,26 +44,19 @@ const vectors = require('./vectors.json')
   console.log('\nhashes are consistent: ', res === refRes && res === jsRes)
 }
 
-{
-  // naive input fuzz
-  const bugs = []
-
+tape('naive input fuzz', function (t) {
   for (let i = 0; i < 10; i++) {
     const buf = crypto.randomBytes(2 ** 18 * Math.random())
 
     const hash = sha512().update(buf).digest('hex')
     const ref = crypto.createHash('sha512').update(buf).digest('hex')
 
-    if (hash !== ref) bugs.push(length)
+    t.ok(hash === ref)
   }
+  t.end()
+})
 
-  console.log('\nhashes inconsistent at lengths:', bugs)
-}
-
-// test power of 2 length buffers
-{
-  const failed = []
-
+tape('test power of 2 length buffers', function (t) {
   for (let i = 0; i < 31; i++) {  
     const hash = sha512()
     const refHash = crypto.createHash('sha512')
@@ -65,14 +66,12 @@ const vectors = require('./vectors.json')
     const test = hash.update(buf).digest('hex')
     const ref = refHash.update(buf).digest('hex')
 
-    if (test !== ref) failed.push(2 ** i)
+    t.ok(test === ref, `2^${i}`)
   }
+  t.end()
+})
 
-  console.log('\nthese lengths failed: ', failed, '\n')
-}
-
-// fuzz multiple updates
-{
+tape('fuzz multiple updates', function (t) {
   const hash = sha512()
   const refHash = crypto.createHash('sha512')
 
@@ -83,25 +82,22 @@ const vectors = require('./vectors.json')
     refHash.update(buf)
   }
 
-  console.log(hash.digest('hex'))
-  console.log(refHash.digest('hex'))
-}
+  t.same(hash.digest(), refHash.digest(), 'multiple updates consistent')
+  t.end()
+})
 
-// crypto-browserify test vectors
-{
-  const failed  = []
-
+tape('crypto-browserify test vectors', function (t) {
+  let i = 0
   for (let vector of vectors) {
     const buf = Buffer.from(vector.input, 'base64')
     const hash = sha512().update(buf).digest('hex')
-    if (hash !== vector.hash) failed.push(vector)
+    t.equal(hash, vector.hash, `failed on input ${i}`)
+    i++
   }
+  t.end()
+})
 
-  console.log('\nthese test vectors failed: ', failed)
-}
-
-// several instances updated simultaneously
-{
+tape('several instances updated simultaneously', function (t) {
   const hash1 = sha512() 
   const hash2 = sha512()
   const refHash = crypto.createHash('sha512')
@@ -125,17 +121,19 @@ const vectors = require('./vectors.json')
   const res1 = hash1.digest('hex')
   const res2 = hash2.digest('hex')
 
-  console.log('\nhashes invariant to update order: ', res === res1 && res1 === res2)
-}
+  t.equal(res, res1, 'consistent with reference')
+  t.equal(res1, res2, 'consistent with eachother')
+  t.end()
+})
 
-// reported bugs
-{
+tape('reported bugs', function (t) {
   const testBuf = Buffer.from('hello')
 
   const res = crypto.createHash('sha512').update(testBuf).digest('hex')
   const res1 = sha512().update(testBuf).digest('hex')
   const res2 = sha512().update(testBuf).digest('hex')
     
-  console.log('\nreported bugs no longer throw: ', res1 === res2 && res1 == res)
-}
-
+  t.equal(res, res1)
+  t.equal(res1, res2)
+  t.end()
+})
