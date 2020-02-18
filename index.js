6c789b41c22e2a17184579e93e9674cedb0eb044
Christophe Diederichs <chris.d.itunes@gmail.com>
2020-02-18 15:27:18 +0100

    fix: digest before update (empty state)

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function Sha512 () {
   this.finalized = false
   this.digestLength = SHA512_BYTES
   this.pointer = freeList.pop()
-  this.leftover
+  this.leftover = Buffer.alloc(0)
 
   wasm.memory.fill(0, this.pointer, this.pointer + 200)
 
